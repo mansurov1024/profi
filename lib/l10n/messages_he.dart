fbbf4072df794fc6b9a0d579b8c5e7dff4c4745a
Igor Mansurov <igorm@crow.co.il>
2019-02-25 15:58:04 +0200

    add project files

---
+++ lib/l10n/messages_he.dart
@@ -0,0 +1,26 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a he locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+// ignore: unnecessary_new
+final messages = new MessageLookup();
+
+// ignore: unused_element
+final _keepAnalysisHappy = Intl.defaultLocale;
+
+// ignore: non_constant_identifier_names
+typedef MessageIfAbsent(String message_str, List args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  get localeName => 'he';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static _notInlinedMessages(_) => <String, Function> {
+    "myOrders" : MessageLookupByLibrary.simpleMessage("ההזמנות שלי"),
+    "title" : MessageLookupByLibrary.simpleMessage("שלום עולם")
+  };
+}
